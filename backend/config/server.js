2a273f3beff6e80642940eac6c79e2c2b8cc4c74
Rômulo Argôlo <ronho@hotmail.co.uk>
2021-07-02 00:37:52 -0300

    New structure

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b1445f493f6339251a009be38ba93d70'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7041a452-6423-446f-8982-3942900fa15f'
+};
